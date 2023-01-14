baf9ed8b44caf3a5d651d4a87dbc59e6e7870f3f
AJ Moon <alex.jared.moon@gmail.com>
2023-01-14 04:02:01 +0000

    initial commit

---
+++ src/lib/evaluation.ts
@@ -0,0 +1,32 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type fetchFunc = () => Promise<any>;
+
+export type TestedFunction = {
+  measure: PerformanceMeasure;
+  data: string;
+};
+
+export function wrapFunction(func: fetchFunc): () => Promise<TestedFunction> {
+  return async () => {
+    performance.mark("startFunctionCall");
+
+    const resp = await func();
+
+    performance.mark("endFunctionCall");
+
+    // if(!resp?.ok) {
+    // 	throw Error("Hmm, this should be a fetch response")
+    // }
+
+    // const data = await resp.text();
+
+    return {
+      data: resp.toString(),
+      measure: performance.measure(
+        "functionCallDuration",
+        "startFunctionCall",
+        "endFunctionCall",
+      ),
+    };
+  };
+}
