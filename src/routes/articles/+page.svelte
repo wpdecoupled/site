d2319056b6e390c1f879e11104345b2ac6440a12
AJ Moon <alex.jared.moon@gmail.com>
2023-03-27 22:31:45 +0000

    wip: articles

---
+++ src/routes/articles/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	/* @type { import('./$houdini').PageData } */
+	export let data;
+
+	$: ({ Articles } = data);
+</script>
+
+<main>
+	<pre>{JSON.stringify($Articles, null, 2)}</pre>
+</main>
