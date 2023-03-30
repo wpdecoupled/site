22ea9ade7afa0f2ddfd1eb82c183913588275011
AJ Moon <alex.jared.moon@gmail.com>
2023-03-30 18:27:24 +0000

    longterm hsts headers since things apear to be good

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
   const response = await resolve(event);
-  response.headers.set('Strict-Transport-Security', 'max-age=3600; includeSubDomains;');
+  response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
 
   return response;
 }
