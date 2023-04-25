4f0c8ce0c2ed1fced2b53d97a9db87cbb6367769
AJ Moon <alex.jared.moon@gmail.com>
2023-04-25 17:43:41 +0000

    fix up analytics to check for ID before executing

--- src/routes/Analytics.svelte
+++ src/routes/Analytics.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-  import { page } from '$app/stores'
-	import { PUBLIC_GA_ID } from '$env/static/public'
+	import { page } from '$app/stores';
+	import { PUBLIC_GA_ID } from '$env/static/public';
 
-  $: {
-    if (typeof gtag !== 'undefined') {
-      gtag('config', PUBLIC_GA_ID, {
-        page_title: document.title,
-        page_path: $page.url.pathname,
-      })
-    }
-  }
+	$: {
+		if (typeof gtag !== 'undefined' && PUBLIC_GA_ID) {
+			gtag('config', PUBLIC_GA_ID, {
+				page_title: document.title,
+				page_path: $page.url.pathname,
+			});
+		}
+	}
 </script>
 
 <svelte:head>
-  <script
-    async
-    src="https://www.googletagmanager.com/gtag/js?id={PUBLIC_GA_ID}">
-  </script>
-  <script>
-    window.dataLayer = window.dataLayer || []
+	{#if PUBLIC_GA_ID}
+		<script async src="https://www.googletagmanager.com/gtag/js?id={PUBLIC_GA_ID}">
+		</script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-      dataLayer.push(arguments)
-    }
+			function gtag() {
+				dataLayer.push(arguments);
+			}
 
-    gtag('js', new Date())
-    gtag('config', PUBLIC_GA_ID)
-  </script>
+			gtag('js', new Date());
+			gtag('config', PUBLIC_GA_ID);
+		</script>
+	{/if}
 </svelte:head>
