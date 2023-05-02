a6982f2f2f9f949e49a7e4e9537491d3388f2234
AJ Moon <alex.jared.moon@gmail.com>
2023-05-02 14:05:55 +0000

    finish rought post layout

--- src/lib/components/wp/post/post.svelte
+++ src/lib/components/wp/post/post.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { formatDate } from '$lib/strings';
 	import { fragment, graphql, type PostPage } from '$houdini';
 
 	export let post: PostPage;
@@ -29,26 +30,92 @@
 	$: author = $data.author?.node;
 </script>
 
-<article class="">
-	<h1>{$data.title}</h1>
-	{#if author}
-		<div>
-			{#if author.avatar}
-				<img
-					src={author.avatar.url}
-					height={author.avatar.height}
-					width={author.avatar.width}
-					alt={author.name}
-				/>
+<article class="prose">
+	<header>
+		<h1>{$data.title}</h1>
+		<p class="top-intro">
+			{author?.name ?? 'Unknown'} on
+			<time datetime={$data.dateGmt}>{formatDate($data.dateGmt)}</time>
+			{#if $data.modifiedGmt !== $data.dateGmt}
+				(Updated <time datetime={$data.modifiedGmt}>{formatDate($data.modifiedGmt)}</time>)
 			{/if}
-			<div>
-				<p>{author.name}</p>
-				{#if author.description}
-					<p>{author.description}</p>
-				{/if}
-			</div>
-		</div>
-	{/if}
+		</p>
+	</header>
 
-	<div class="prose">{@html $data.content}</div>
+	<div class="content">{@html $data.content}</div>
+
+	<footer>
+		{#if author}
+			<div class="author-card">
+				{#if author.avatar}
+					<img
+						src={author.avatar.url}
+						height={author.avatar.height}
+						width={author.avatar.width}
+						alt={author.name}
+						loading="lazy"
+					/>
+				{/if}
+				<div>
+					<h2>About {author.name}</h2>
+					{#if author.description}
+						<p>{author.description}</p>
+					{/if}
+				</div>
+			</div>
+		{/if}
+	</footer>
 </article>
+
+<style lang="postcss">
+	article {
+		margin: auto;
+	}
+
+	header,
+	.content {
+		margin-bottom: var(--size-fluid-4);
+	}
+
+	.top-intro {
+		font-size: smaller;
+		padding-top: var(--size-fluid-1);
+	}
+
+	footer {
+		border: 1px dashed var(--primary-color);
+		/* border-radius: var(--radius-1); */
+		padding: var(--size-fluid-1);
+	}
+
+	.author-card {
+		display: flex;
+		align-items: center;
+	}
+
+	.author-card img {
+		border-radius: 50%;
+		height: 100%;
+		margin-right: var(--size-fluid-1);
+	}
+
+	:global(.prose .content p) {
+		padding-bottom: var(--size-fluid-1);
+		line-height: var(--font-lineheight-4);
+		letter-spacing: var(--font-letterspacing-1);
+	}
+
+	:global(.prose .content li) {
+		padding-bottom: var(--size-fluid-1);
+		line-height: var(--font-lineheight-2);
+		letter-spacing: var(--font-letterspacing-1);
+
+	}
+
+	:global(.prose .content h2) {
+		padding-bottom: var(--size-fluid-2);
+		font-size: var(--font-size-5);
+		line-height: var(--font-lineheight-4);
+
+	}
+</style>
