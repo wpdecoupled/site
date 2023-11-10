30a024d85b61ba74ede6d21736be83703481e02c
Alex Moon <moonmeister@users.noreply.github.com>
2023-11-10 14:49:09 +0000

    wip: guides

---
+++ src/routes/guides/[slug]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Post from '$lib/components/wp/post/post.svelte';
+
+	import type { PageData } from './$houdini';
+
+	export let data: PageData;
+
+	$: ({ Article } = data);
+</script>
+
+<main class="wrapper">
+	{#if $Article.fetching}
+		<p>Loading...</p>
+	{:else if !$Article.data?.post}
+		<p>No article found</p>
+	{:else}
+		<Post post={$Article.data.post} />
+	{/if}
+</main>
