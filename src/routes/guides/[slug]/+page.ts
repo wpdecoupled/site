7fc86d43e5188503d4dd5855043d14d186e09664
AJ Moon <alex.jared.moon@gmail.com>
2023-12-11 18:24:19 +0000

    fix: add missing disable csr to guides

--- src/routes/guides/[slug]/+page.ts
+++ src/routes/guides/[slug]/+page.ts
@@ -2,6 +2,9 @@ import { error } from '@sveltejs/kit';
 
 import type { AfterLoadEvent } from './$houdini';
 
+export const csr = false;
+
+
 export function _houdini_afterLoad({ data }: AfterLoadEvent) {
 	if (!data.Guide) {
 		throw error(404);
