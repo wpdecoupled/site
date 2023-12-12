102f2993bde3f8594900bd0cfac2686d3b8334f5
AJ Moon <alex.jared.moon@gmail.com>
2023-12-12 21:11:32 +0000

    feat: implement search on error page and throw error on empty search query

--- src/routes/search/+page.ts
+++ src/routes/search/+page.ts
@@ -1,11 +1,19 @@
 import type { PageLoad } from './$types';
 import { load_SearchQuery } from '$houdini';
+import { error } from '@sveltejs/kit';
+import { StatusCodes } from 'http-status-codes';
 
 export const prerender = false;
 
 export const load: PageLoad = async (event) => {
 	const { url } = event;
-	const query = url.searchParams.get('q') ?? '';
+	const query = url.searchParams.get('q');
+
+	if (!query) {
+		throw error(StatusCodes.BAD_REQUEST, {
+			message: 'Missing search query',
+		})
+	}
 
 	const resp = await load_SearchQuery({ event, variables: { query } });
 
