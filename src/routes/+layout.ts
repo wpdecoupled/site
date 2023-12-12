b90882b9be47ae705ff5493b1582c610de092c71
AJ Moon <alex.jared.moon@gmail.com>
2023-12-12 20:00:50 +0000

    feat: add caching headers

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,10 +1,21 @@
-import type { LayoutQueryVariables } from './$houdini';
+import { load_LayoutQuery } from '$houdini';
+
+import type { LayoutLoad } from './$types';
 
 export const trailingSlash = 'always';
 export const ssr = true;
 
-export const _LayoutQueryVariables: LayoutQueryVariables = (event) => {
+export const load: LayoutLoad = async (event) => {
+	event.setHeaders({
+		'cache-control': 'public, max-age=20, stale-while-revalidate=60, stale-if-error=86400',
+	});
+
 	return {
-		uri: event.url.pathname,
+		...(await load_LayoutQuery({
+			event,
+			variables: {
+				uri: event.url.pathname,
+			},
+		})),
 	};
 };
