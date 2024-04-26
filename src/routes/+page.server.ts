dd63c832d9dda525b05c38f9b57c404555cb7d0f
AJ Moon <alex.jared.moon@gmail.com>
2024-04-26 05:47:07 +0000

    fix: form (also schema updated cause things

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,15 @@
 import type { Actions } from './$types';
 import { PUBLIC_WP_URL } from '$env/static/public';
 import { fail } from '@sveltejs/kit';
+import { formatDate } from '$lib/strings';
 
 const FORM_URL = PUBLIC_WP_URL + '/wp-json/contact-form-7/v1/contact-forms/11/feedback';
 
 export const actions = {
-	default: async (event) => {
-		const data = await event.request.formData();
+	default: async ({ request }) => {
+		const data = await request.formData();
+
+		data.append('_wpcf7_unit_tag', `wpcf7-f11-${crypto.randomUUID()}`);
 
 		const req = await fetch(FORM_URL, {
 			method: 'POST',
