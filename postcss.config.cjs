8e2a6cc8a9eed33d736598caf0f2c492d3d9108b
AJ Moon <alex.jared.moon@gmail.com>
2024-05-03 01:56:21 +0000

    refactor: postcss config to esm

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-const autoprefixer = require('autoprefixer');
-const postcssCustomMedia = require('postcss-custom-media');
-const postcssNesting = require('postcss-nesting');
-
-const config = {
-	plugins: [postcssCustomMedia, postcssNesting, autoprefixer],
-};
-
-module.exports = config;
---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+import  autoprefixer from 'autoprefixer';
+import postcssNesting  from 'postcss-nesting';
+import postcssCustomMedia from 'postcss-custom-media';
+
+const config = {
+	plugins: [postcssCustomMedia, postcssNesting, autoprefixer],
+};
+
+export default config;
