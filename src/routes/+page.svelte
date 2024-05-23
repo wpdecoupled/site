0ff6c91806f809183d6ca33871ccb00dabae57f0
AJ Moon <alex.jared.moon@gmail.com>
2024-05-23 05:12:30 +0000

    refactor: move homepage content to fetch from WP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,43 +4,45 @@
 	import RecentPosts from '$lib/components/wp/content/recent-posts.svelte';
 	import PostMiniCard from '$lib/components/wp/post/post-mini-card.svelte';
 	import type { PageData } from './$houdini';
+	import { HomePageQueryStore } from '$houdini';
 
 	export let form: ActionData;
 
 	export let data: PageData;
 
-	$: ({ RecentPostsQuery } = data);
+	$: ({ HomePageQuery } = data);
 </script>
 
 <main class="wrapper">
-	<section class="about center add-some-spacing">
-		<h1>Independent Decoupled WordPress Content</h1>
-		<br />
-		<p class="">
-			Come join us on this headless journey! Let us know what you want read about next on <a
-				href="https://twitter.com/intent/post?text=%40wpdecoupled%20"
-				target="_blank"
-				rel="nofollow noreferrer noopener">Twitter</a
-			>
-			or by <a href="mailto:editor@wpdecouled.dev">email</a>.
-		</p>
-	</section>
-	<section class="recent-posts">
-		<RecentPosts>
-			{#if $RecentPostsQuery.fetching}
-				<p>Loading...</p>
-			{:else if !$RecentPostsQuery.data?.contentNodes?.nodes}
-				<p>No articles found</p>
+	{#if $HomePageQuery.fetching}
+		<p>Loading...</p>
+	{:else}
+		<section class="about center add-some-spacing">
+			{#if !$HomePageQuery.data?.page}
+				<p>No content found</p>
 			{:else}
-				{#each $RecentPostsQuery.data?.contentNodes?.nodes as post}
-					<PostMiniCard {post} />
-				{/each}
+				<h1>{$HomePageQuery.data.page.title}</h1>
+				<br />
+				<p>
+					{@html $HomePageQuery.data.page.content.replaceAll(/<\/?p>/g, '')}
+				</p>
 			{/if}
-		</RecentPosts>
-	</section>
-	<section id="signup" class="center add-some-spacing">
-		<EmailForm {form} />
-	</section>
+		</section>
+		<section class="recent-posts">
+			<RecentPosts>
+				{#if !$HomePageQuery.data?.contentNodes?.nodes}
+					<p>No articles found</p>
+				{:else}
+					{#each $HomePageQuery.data?.contentNodes?.nodes as post}
+						<PostMiniCard {post} />
+					{/each}
+				{/if}
+			</RecentPosts>
+		</section>
+		<section id="signup" class="center add-some-spacing">
+			<EmailForm {form} />
+		</section>
+	{/if}
 </main>
 
 <style lang="postcss">
