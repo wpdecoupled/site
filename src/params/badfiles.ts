775a77837db97d8215896c77b860085f92891410
Alex Moon <moonmeister@users.noreply.github.com>
2024-10-04 14:07:10 -0700

    expand bad file names null route

---
+++ src/params/badfiles.ts
@@ -0,0 +1,19 @@
+import type { ParamMatcher } from '@sveltejs/kit';
+
+// This function is used to match the parameter in the route
+/**
+example.php
+example.php/
+.example
+example.PhP7
+class-snoopi.php.suspected
+example.html
+test.css
+*/
+
+const regex = /(.php)|^\./gmi;
+
+export const match: ParamMatcher = (param) => {
+	const standard = param.toLowerCase();
+	return regex.test(standard);
+};
